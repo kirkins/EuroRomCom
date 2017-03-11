b4487d182ff21863b49407e6d0d5fcdf2c453393
Philip Kirkbride <kirkins@gmail.com>
2017-03-11 14:06:48 -0500

    improve quiz

--- games/simpleQuiz/js/scripts.js
+++ games/simpleQuiz/js/scripts.js
@@ -19,20 +19,36 @@ function getWordSet(lang1, lang2) {
   return wordSet;
 }
 
-// Event listners
-$("#startGame").click(function() {
+function shuffle(a) {
+  var j, x, i;
+  for (i = a.length; i; i--) {
+    j = Math.floor(Math.random() * i);
+    x = a[i - 1];
+    a[i - 1] = a[j];
+    a[j] = x;
+  }
+  return a;
+}
+
+function nextQuestion() {
   var question = getWordSet($('#lang1').val(),$('#lang2').val());
+  var answerButtons = [1,2,3,4];
+  answerButtons = shuffle(answerButtons); 
   for (i = 0; i < 4; i++) {
-    eval("$('#a"+(i+1)+"').html(question.answers["+i+"])");
+    eval("$('#a"+answerButtons[i]+"').html(question.answers["+i+"])");
   }
   $('#question').html(question.question);
   currentAnswer = question.correct;
-});
+}
+
+// Event listners
+$("#startGame").click(function() { nextQuestion() });
 
 $(".answer").click(function() {
   if($(this).html()==currentAnswer){
-    alert("correct");
+    $('#result').html('Right!');
   } else {
-    alert("false");
+    $('#result').html('Wrong');
   }
+  nextQuestion();
 });
